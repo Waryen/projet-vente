8e7563c441f4a8867b0216843c738d19a6bc99d9
Jonathan Gomand <jonathangomand@gmail.com>
2020-10-23 17:36:05 +0200

    Décomposition de homepage en components, finalisation de choix-item

---
+++ src/components/categorie/categorie.component.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './categorie.style.scss'
+import ChoixItem from '../choix-item/choix-item.component.jsx'
+
+class Categorie extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+        sections : [
+            {
+              titre: 'chapeaux',
+              imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+              id: 1
+            },
+            {
+              titre: 'vestes',
+              imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+              id: 2
+            },
+            {
+              titre: 'baskets',
+              imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+              id: 3
+            },
+            {
+              titre: 'femmes',
+              imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+              taille: 'large',
+              id: 4
+            },
+            {
+              titre: 'hommes',
+              imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+              taille: 'large',
+              id: 5
+            }
+          ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="categories">
+        {
+            this.state.sections.map(({titre, imageUrl, taille, id}) => (
+                <ChoixItem key={id} titre={titre} imageUrl={imageUrl} taille={taille} />
+            ))
+        }
+      </div>
+    )
+  }
+}
+
+export default Categorie
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './homepage.style.scss';
+import Categorie from '../../components/categorie/categorie.component.jsx'
+
+const HomePage = () => (
+  <div className="homepage">
+    <Categorie />
+  </div>
+)
+
+export default HomePage
